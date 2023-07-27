6aeb61cd1934697400243b28fa5e22f9684f6b97
VladimirKholodovskiy <silich2@mail.ru>
2023-07-28 01:36:12 +0600

    props

---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+/* eslint-disable react/prop-types */
+export function Button (props) {
+    console.log(props);
+    return <button>{props.text}</button>
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
 import ReactDOM from 'react-dom/client'
 
-
+import { Button } from './components/Button'
+const text = 'clickMe'
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render('4444')
+root.render(
+  <div>
+    <Button text={text + 1}/>
+    <Button text={text + 2}/>
+    <Button text={text + 3}/>
+  </div>
+)
